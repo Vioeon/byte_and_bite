ed2db163ea4537454862c712577f93690ce81812
Lee SeungEon <dltmddjs98@naver.com>
2026-07-24 21:12:19 +0900

    init: 프로젝트 초기 생성

---
+++ byte-bite/src/main/java/net/likelion/bebc25/bytebite/ByteBiteApplication.java
@@ -0,0 +1,13 @@
+package net.likelion.bebc25.bytebite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteBiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByteBiteApplication.class, args);
+    }
+
+}
---
+++ byte-bite/src/test/java/net/likelion/bebc25/bytebite/ByteBiteApplicationTests.java
@@ -0,0 +1,13 @@
+package net.likelion.bebc25.bytebite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteBiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
