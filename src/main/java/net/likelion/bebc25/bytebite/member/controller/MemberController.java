352945b04f453bbcf3ac2fd8225f7859949f9c4a
Lee SeungEon <dltmddjs98@naver.com>
2026-08-02 01:31:54 +0900

    Design: 디자인 수정

--- src/main/java/net/likelion/bebc25/bytebite/member/controller/MemberController.java
+++ src/main/java/net/likelion/bebc25/bytebite/member/controller/MemberController.java
@@ -34,10 +34,10 @@ public class MemberController {
 
             // memberId로 식당 정보 조회하여 Dto에 저장
             RestaurantDto restaurant = restaurantService.findByMemberId(loginMember.getMemberId());
-
+            log.info(restaurant.getImageUrl());
             model.addAttribute("restaurant", restaurant);
         }
-
+        log.info(loginMember.getNickname());
         model.addAttribute("menu", "mypage");
         return "mypage/mypage";
     }
@@ -114,6 +114,8 @@ public class MemberController {
         // 세션 생성하여 사용자 정보 저장
         SessionMemberDto sessionMember = new SessionMemberDto(memberInfo);
         session.setAttribute("loginMember", sessionMember);
+        // 로그인한 세션 30분 설정
+        session.setMaxInactiveInterval(1800);
 
         return "redirect:/posts";
     }
@@ -142,7 +144,6 @@ public class MemberController {
 
         // 사용한 세션 제거
         session.removeAttribute("signupForm");
-
         return "redirect:/member/login";
     }
 
--- src/main/java/net/likelion/bebc25/bytebite/member/dto/MemberDto.java
+++ src/main/java/net/likelion/bebc25/bytebite/member/dto/MemberDto.java
@@ -22,18 +22,12 @@ public class MemberDto {
     private int memberId;
 
     // 닉네임
-    @NotBlank(message = "닉네임은 필수 입력 항목입니다.")
-    @Size(min = 2, max = 20, message = "닉네임 2자 이상 20자 이하여야 합니다.")
     private String nickname;
 
     // 이메일
-    @NotBlank(message = "이메일은 필수 입력 항목입니다.")
-    @Size(min = 4, max = 50, message = "이메일은 4자 이상 50자 이하여야 합니다.")
     private String email;
 
     // 비밀번호
-    @NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
-    @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하여야 합니다.")
     private String password;
 
     private String role;
