028ca62bea4dde15383bd57190cea739d6803682
Lee SeungEon <dltmddjs98@naver.com>
2026-08-04 11:59:09 +0900

    식당 테스트 이미지 추가 및 gitignore수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -14,19 +14,19 @@ DROP TABLE IF EXISTS member;
 # SELECT post_id, image
 # FROM post;
 
-SELECT restaurant_id, rname
-FROM restaurant;
-
-SELECT restaurant_id, rname
-FROM restaurant
-WHERE rname LIKE '%없는식당%';
-
-SELECT restaurant_id, rname
-FROM restaurant;
-
-SELECT restaurant_id, rname
-FROM restaurant
-WHERE rname LIKE '%없는식당%';
+# SELECT restaurant_id, rname
+# FROM restaurant;
+#
+# SELECT restaurant_id, rname
+# FROM restaurant
+# WHERE rname LIKE '%없는식당%';
+#
+# SELECT restaurant_id, rname
+# FROM restaurant;
+#
+# SELECT restaurant_id, rname
+# FROM restaurant
+# WHERE rname LIKE '%없는식당%';
 
 ALTER TABLE post
     MODIFY COLUMN image TEXT;
